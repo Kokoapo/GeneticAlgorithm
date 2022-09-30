b4b56a47534a64af4e16301fdad503cca92cdf76
Kokoapo <viniviscdiniz@gmail.com>
2022-09-30 09:59:24 -0300

    Added Setters Methods

--- src/com/yes/main/Pops.java
+++ src/com/yes/main/Pops.java
@@ -16,11 +16,23 @@ public class Pops {
         return x;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
     public int getY() {
         return y;
     }
 
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public int getFitness() {
         return fitness;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
     }
 }
